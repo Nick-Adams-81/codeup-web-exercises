9119ada168a7b31e8ab7881e7c319ab16e0cf779
nick adams <thebigdeal916@icloud.com>
2021-08-12 16:15:23 -0500

    feat: added prompts for the movie rental activity, exercises on 8-12

--- js/external.js
+++ js/external.js
@@ -1,33 +1,32 @@
 'use strict';
 
-console.log('hello from external.js');
-console.log('this is definitely working');
-alert("Welcome to my website!")
+// console.log('hello from external.js');
+// alert("Welcome to my website!");
+//
+// var color = prompt('What is your favorite color?');
+// alert('Awesome, ' + color + ' is my favorite color too!');
 
-var color = prompt('What is your favorite color?');
-alert('Awesome, ' + color + ' is my favorite color too!');
-
-var a = 3;
-var b = 5;
-var c = 1;
-var sum = (a + b + c) * 3;
+var a = prompt('How long would you like to rent the Little Mermaid?');
+var b = prompt('How long are you renting Brother Bear for?');
+var c = prompt('Hercules huh? How many days are you renting it for?');
+var sum = ((a * 3) + (b * 3) + (c * 3));
 console.log(sum);
 
-var google = prompt('input your google pay');
-var facebook = prompt('input your facebook pay');
-var amazon = prompt('input your amazon pay');
-var total = (google * 6) + (facebook * 10) + (amazon * 4);
-console.log(total);
-
-var classTotal = prompt('How many students are in the classroom?');
-var fullClassTotal = prompt('whats the max number of students allowed in the classroom?');
-var studentSchedule = ['Saturday', 'Sunday']
-var classSchedule = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-var canAttend = studentSchedule != classSchedule && fullClassTotal > classTotal;
-console.log(canAttend);
-
-var item = 1;
-var productOffer = item > 2;
-var premium = item >= 1;
-console.log(productOffer, premium)
-
+// var google = prompt('input your google pay');
+// var facebook = prompt('input your facebook pay');
+// var amazon = prompt('input your amazon pay');
+// var total = (google * 6) + (facebook * 10) + (amazon * 4);
+// console.log(total);
+//
+// var classTotal = prompt('How many students are in the classroom?');
+// var fullClassTotal = prompt('whats the max number of students allowed in the classroom?');
+// var studentSchedule = ['Saturday', 'Sunday']
+// var classSchedule = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+// var canAttend = studentSchedule != classSchedule && fullClassTotal > classTotal;
+// console.log(canAttend);
+//
+// var item = 1;
+// var productOffer = item > 2;
+// var premium = item >= 1;
+// console.log(productOffer, premium)
+//
