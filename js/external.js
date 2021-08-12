13d48af7cbd5b86c7ce1264e972b11efafb6d888
nick adams <thebigdeal916@icloud.com>
2021-08-12 14:53:36 -0500

    feat: updated for our exercise today

--- js/external.js
+++ js/external.js
@@ -1,2 +1,8 @@
-console.log('hello from external js')
-console.log('this is def working')
+'use strict';
+
+console.log('hello from external.js');
+console.log('this is definitely working');
+alert("Welcome to my website!")
+
+let color = prompt('What is your favorite color?')
+alert('Awesome, ' + color + ' is my favorite color too!')
