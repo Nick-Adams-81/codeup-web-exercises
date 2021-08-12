599abf6e7679dd74dbd531ded9ed9b0961f65ddb
nick adams <thebigdeal916@icloud.com>
2021-08-12 16:03:07 -0500

    code refactor, added prompts for class exercise for 8-12

--- js/external.js
+++ js/external.js
@@ -1,35 +1,33 @@
 'use strict';
 
-// console.log('hello from external.js');
-// console.log('this is definitely working');
-// alert("Welcome to my website!")
-//
-// var color = prompt('What is your favorite color?');
-// alert('Awesome, ' + color + ' is my favorite color too!');
-//
-// var a = 3;
-// var b = 5;
-// var c = 1;
-// var sum = (a + b + c) * 3;
-// console.log(sum);
+console.log('hello from external.js');
+console.log('this is definitely working');
+alert("Welcome to my website!")
 
-var google = 400;
-var facebook = 350;
-var amazon = 380;
+var color = prompt('What is your favorite color?');
+alert('Awesome, ' + color + ' is my favorite color too!');
+
+var a = 3;
+var b = 5;
+var c = 1;
+var sum = (a + b + c) * 3;
+console.log(sum);
+
+var google = prompt('input your google pay');
+var facebook = prompt('input your facebook pay');
+var amazon = prompt('input your amazon pay');
 var total = (google * 6) + (facebook * 10) + (amazon * 4);
 console.log(total);
 
-var classTotal = 0
-var fullClassTotal = 27
+var classTotal = prompt('How many students are in the classroom?');
+var fullClassTotal = prompt('whats the max number of students allowed in the classroom?');
 var studentSchedule = ['Saturday', 'Sunday']
 var classSchedule = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
 var canAttend = studentSchedule != classSchedule && fullClassTotal > classTotal;
-console.log(canAttend)
+console.log(canAttend);
 
 var item = 1;
 var productOffer = item > 2;
 var premium = item >= 1;
-
 console.log(productOffer, premium)
-
 
