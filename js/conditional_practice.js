1298e0c9effa1d09d0b4a4330bf02ff3d3124efe
nick adams <thebigdeal916@icloud.com>
2021-08-16 13:55:00 -0500

    added ternary operator and switch case

--- js/conditional_practice.js
+++ js/conditional_practice.js
@@ -4,8 +4,8 @@
     // ---------- CONDITIONALS ---------- //
 
     // if statement //
-    var isSunny = confirm('is it sunny today?');
-    var hasSunScreen = confirm('do you have sunscreen?')
+    // var isSunny = confirm('is it sunny today?');
+    // var hasSunScreen = confirm('do you have sunscreen?')
     // if(isSunny) {
     //     alert('Don\'t forget the sunscreen');
     // }
@@ -17,14 +17,60 @@
     //     alert('No need for sunscreen today');
     // }
 
-    // if/else if/else statement
-    if(isSunny && hasSunScreen) {
-        alert('Great! You brought sunscreen')
-    } else if(isSunny && !hasSunScreen) {
-        alert('Be careful, it\'s a sunny day! Don\'t get burnt!!!');
-    } else {
-        alert('it\'s not too sunny you will be fine!');
+    // if, else-if, else statement
+    // if(isSunny && hasSunScreen) {
+    //     alert('Great! It\'s sunny and You brought sunscreen')
+    // } else if(isSunny && !hasSunScreen) {
+    //     alert('Be careful, it\'s a sunny day! Don\'t get burnt!!!');
+    // } else {
+    //     alert('it\'s not too sunny you will be fine!');
+    // }
+
+    // traffic light example(nested if statements) //
+
+    // var trafficLight = prompt('What color is the traffic light?');
+    // // making the user input all lowercase
+    // trafficLight = trafficLight.toLowerCase()
+    // if(trafficLight === 'red') {
+    //     var blinking = confirm('Is the red light blinking?');
+    //     if(blinking){
+    //         alert('Red light is blinking');
+    //     }
+    //     alert("STOP!!! The light is red");
+    // } else if(trafficLight === "yellow") {
+    //     alert('careful the light is yellow');
+    // } else if(trafficLight === 'green') {
+    //     alert('The light is green you\'re good to go!!!');
+    // } else {
+    //     alert('the light is out mind your surroundings');
+    // }
+
+    // ternary operators (shorthand if/else) //
+
+    (true) ? console.log('operation was successful') : console.log('Oops something went wrong');
+
+    // Switch statements //
+
+    var pizzaPref = prompt('What\'s your favorite pizza?');
+    pizzaPref = pizzaPref.toLowerCase()
+    switch(pizzaPref) {
+        case 'pepperoni':
+            alert('Pepperoni is my favorite too!!!');
+            break;
+        case 'cheese':
+            alert('Cheese is pretty basic but cool!');
+            break;
+        case "chicken":
+            alert('Nothing wrong with chicken!');
+            break;
+        default:
+            alert(pizzaPref + ' isn\'t my fav but not judging');
+            break;
+
     }
+
+
+
 
 
 })();
