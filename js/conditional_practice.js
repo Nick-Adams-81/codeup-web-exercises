0d2c48c2081ce3d77d62a5f7d72693a91829be95
nick adams <thebigdeal916@icloud.com>
2021-08-16 11:04:32 -0500

    added: first if and if/else statements

--- js/conditional_practice.js
+++ js/conditional_practice.js
@@ -1,6 +1,22 @@
 'use strict';
 
 (function() {
-    console.log('testing');
+    // ---------- CONDITIONALS ---------- //
+
+    // basic if statement //
+    var isSunny = confirm('is it sunny today?')
+    // if(isSunny) {
+    //     alert('Don\'t forget the sunscreen');
+    // }
+
+    // basic if/else statement //
+    if(isSunny) {
+        alert('Bring your sunscreen');
+    } else {
+        alert('No need for sunscreen today');
+    }
+
+
+
 
 })();
