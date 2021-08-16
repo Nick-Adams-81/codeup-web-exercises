bd38123d2fdc1fef7f49a132308b2316552a036c
nick adams <thebigdeal916@icloud.com>
2021-08-16 11:15:10 -0500

    added first else if statement

--- js/conditional_practice.js
+++ js/conditional_practice.js
@@ -3,20 +3,28 @@
 (function() {
     // ---------- CONDITIONALS ---------- //
 
-    // basic if statement //
-    var isSunny = confirm('is it sunny today?')
+    // if statement //
+    var isSunny = confirm('is it sunny today?');
+    var hasSunScreen = confirm('do you have sunscreen?')
     // if(isSunny) {
     //     alert('Don\'t forget the sunscreen');
     // }
 
-    // basic if/else statement //
-    if(isSunny) {
-        alert('Bring your sunscreen');
+    // if/else statement //
+    // if(isSunny) {
+    //     alert('Bring your sunscreen');
+    // } else {
+    //     alert('No need for sunscreen today');
+    // }
+
+    // if/else if/else statement
+    if(isSunny && hasSunScreen) {
+        alert('Great! You brought sunscreen')
+    } else if(isSunny && !hasSunScreen) {
+        alert('Be careful, it\'s a sunny day! Don\'t get burnt!!!');
     } else {
-        alert('No need for sunscreen today');
+        alert('it\'s not too sunny you will be fine!');
     }
-
-
 
 
 })();
