f12f28966d41b8ce3cbb0595c767b5cb22c4318c
nick adams <thebigdeal916@icloud.com>
2021-08-18 09:57:31 -0500

    do while loop added

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -6,22 +6,22 @@
     // this isnt working yet, further research is needed.
     var number = prompt('Please pick an odd number between 1 and 50');
     for(var i = 1; i < 50; i++) {
-        while(number % 2 === 0) {
+        if(number % 2 === 0) {
             var newNum = prompt('please pick an odd number');
             alert('your number is ' + newNum);
             break;
-        } if(number % 2 !== 0) {
+        }else  {
             alert('your number is ' + number);
             break;
         }
 
     }
     // looping through continuing past user input.
+    console.log('Number to skip is: ' + number || newNum);
     for(var j = 1; j < 50; j++) {
         if(j % 2 === 0) {
             continue;
-
-        }else if(j == number) {
+        } else if(j == number) {
             console.log('Wer\'e gonna skip this number: ' + j);
         } else {
             console.log('current number: ' + j);
---
+++ js/while.js
@@ -0,0 +1,24 @@
+'use strict';
+
+(function() {
+
+
+    var number = 1;
+    while(number <= 65536) {
+       if(number % 2 === 0) console.log(number);
+       number = number * 2;
+    }
+
+    var conesToSell = Math.floor(Math.random() * 50) + 50;
+    console.log('there are ' + conesToSell + ' left to sell');
+    do {
+        var conesSold = Math.floor(Math.random() * 5) + 1;
+        console.log('there\'s ' + conesSold + ' cones that have been sold');
+        if(conesToSell >= conesSold) {
+            conesToSell = conesToSell - conesSold;
+             console.log('you have ' + conesToSell + ' more cones to sell');
+        }
+    } while(conesToSell > 0);
+    console.log('You sold all the cones');
+
+})()
