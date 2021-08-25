ed788eaa9c304dd911977cb051141bdc6c8cf59d
nick adams <thebigdeal916@icloud.com>
2021-08-25 11:44:08 -0500

    added Math sqrt function

--- js/math-lec.js
+++ js/math-lec.js
@@ -32,3 +32,10 @@ console.log(Math.floor(4.9));
 // Math.ceil rounds a number up
 console.log(Math.ceil(4.2));
 
+// Math.pow returns the base to the exponent power(2, 16) returns 65536
+
+console.log(Math.pow(2, 16));
+
+// Math.sqrt returns the square root of a number
+
+console.log(Math.sqrt(9));
