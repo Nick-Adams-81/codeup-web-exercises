ed99a9189ae7317fe1e5a3b92411de7e3b92b7e8
nick adams <thebigdeal916@icloud.com>
2021-08-25 10:01:59 -0500

    added Math.floor and Math.random functions

--- js/math-lec.js
+++ js/math-lec.js
@@ -3,10 +3,21 @@
 // warm up
 function rolledSixSided(number) {
     for(var i = 0; i < number; i++) {
-       var num = Math.floor(Math.random() * (6 - 1) + 1);
+       var num = Math.floor(Math.random() * (6 - 1 + 1) + 1);
        console.log(num);
     }
 
 }
+rolledSixSided(10);
 
-rolledSixSided(6);
+
+// Math methods //
+
+//Math.random will give you a random number
+
+console.log(Math.random());
+
+// how to get a random number in a specified range with Math.floor and Math.random
+var max = 10;
+var min = 2;
+console.log(Math.floor(Math.random() * (max - min + min) + min));
