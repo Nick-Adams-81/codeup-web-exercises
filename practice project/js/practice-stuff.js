24dc4b26aa6371f4dcdb4fd24fdd3b2f7c69a2f3
nick adams <thebigdeal916@icloud.com>
2021-08-28 08:06:56 -0500

    image tag added

--- practice project/js/practice-stuff.js
+++ practice project/js/practice-stuff.js
@@ -11,17 +11,24 @@ fetch('https://rickandmortyapi.com/api/character/')
 let showCharacters = characters => {
     const characterDiv = document.querySelector('#rick-and-morty-characters');
     const characterDiv2 = document.querySelector('#rick-and-morty-status');
-    const speciesDiv = document.querySelector('#rick-and-morty-species')
+    const speciesDiv = document.querySelector('#rick-and-morty-species');
+    const  imageDiv = document.querySelector('#img')
+
+
     characters.forEach(character => {
         const characterElement = document.createElement('p');
         const characterEl = document.createElement('p');
         const charEl2 = document.createElement('p');
+        const charImg = document.createElement('img');
         characterElement.innerText = `Name: ${character.name}`;
         characterEl.innerText = `Status: ${character.status}`
         charEl2.innerText = `Species: ${character.species}`
+        charImg.image = `Img: ${character.image}`
         characterDiv.append(characterElement);
         characterDiv2.append(characterEl);
         speciesDiv.append(charEl2);
+        imageDiv.append(charImg);
+
 
     })
 
