46b04aa38eab34c7657b5e6c11162c7ec9da648d
nick adams <thebigdeal916@icloud.com>
2021-08-27 19:51:58 -0500

    added some content to html page using fetch

--- js/practice-stuff.js
+++ js/practice-stuff.js
@@ -8,11 +8,28 @@
 
  // fetch api
 
-fetch('https://jsonplaceholder.typicode.com/users')
+fetch('https://rickandmortyapi.com/api/character/')
     .then(res => res.json())
-    .then(data => console.log(data))
+
+    .then(characters => showCharacters(characters.results))
     .catch(err => console.log(err))
 
 
 
+let showCharacters = characters => {
+    const characterDiv = document.querySelector('#rick-and-morty-characters');
 
+    characters.forEach(character => {
+        const characterElement = document.createElement('p');
+        characterElement.innerText = `Name: ${character.name}`;
+        characterDiv.append(characterElement);
+    })
+
+    const characterDiv2 = document.querySelector('#rick-and-morty-status');
+    characters.forEach(char => {
+        const characterEl = document.createElement('p');
+        characterEl.innerText = `Status: ${char.status}`
+        characterDiv2.append(characterEl);
+    })
+
+}
