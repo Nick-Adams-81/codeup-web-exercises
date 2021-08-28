f0ba47c2676ceb64c525e8bd985ce46d05485512
nick adams <thebigdeal916@icloud.com>
2021-08-27 19:24:45 -0500

    fetch function test

--- js/practice-stuff.js
+++ js/practice-stuff.js
@@ -1,7 +1,18 @@
 'use strict';
 
+// random event listener
  var btn = document.getElementById('btn');
  btn.addEventListener('click', function() {
      alert('i got clicked')
- })
+ });
+
+ // fetch api
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+
+
+
 
