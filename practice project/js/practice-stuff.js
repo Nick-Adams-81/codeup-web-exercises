eac0353a096b124cadc50fd79a37b56100b53c0a
nick adams <thebigdeal916@icloud.com>
2021-08-28 08:17:48 -0500

    practice folder added

--- practice project/js/practice-stuff.js
+++ practice project/js/practice-stuff.js
@@ -12,7 +12,7 @@ let showCharacters = characters => {
     const characterDiv = document.querySelector('#rick-and-morty-characters');
     const characterDiv2 = document.querySelector('#rick-and-morty-status');
     const speciesDiv = document.querySelector('#rick-and-morty-species');
-    const  imageDiv = document.querySelector('#img')
+    const  imageDiv = document.querySelector('#img');
 
 
     characters.forEach(character => {
@@ -21,15 +21,13 @@ let showCharacters = characters => {
         const charEl2 = document.createElement('p');
         const charImg = document.createElement('img');
         characterElement.innerText = `Name: ${character.name}`;
-        characterEl.innerText = `Status: ${character.status}`
-        charEl2.innerText = `Species: ${character.species}`
-        charImg.image = `Img: ${character.image}`
+        characterEl.innerText = `Status: ${character.status}`;
+        charEl2.innerText = `Species: ${character.species}`;
+        charImg.image = `Img: ${character.image}`;
         characterDiv.append(characterElement);
         characterDiv2.append(characterEl);
         speciesDiv.append(charEl2);
         imageDiv.append(charImg);
-
-
-    })
+    });
 
 }
---
+++ practice stuff/js/practice.js
@@ -0,0 +1,14 @@
+'use strict';
+
+(function() {
+
+    var me = {
+        name: 'Nick Adams',
+        where: 'Dallas, TX',
+        age: 39
+
+    }
+
+    console.log(me);
+
+})()
