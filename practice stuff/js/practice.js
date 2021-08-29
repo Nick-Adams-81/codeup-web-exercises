4e3af5518680eb4af307b7f3a819189e249c22ad
nick adams <thebigdeal916@icloud.com>
2021-08-29 18:25:22 -0500

    more objects added for practice

--- practice stuff/js/practice.js
+++ practice stuff/js/practice.js
@@ -5,17 +5,46 @@
     var me = {
         name: 'Nick Adams',
         where: 'Dallas, TX',
-        age: 39
+        age: '39'
 
     }
 
-    me.isSingle = true;
+    me.isSingle = false;
     me.myCar = {
         make: 'Tesla',
         model: 3,
         year: 2019
     }
 
+
+    var myGirl = {
+            name: 'Jenny Marin',
+            where: 'Dallas, TX',
+            age: '22'
+
+        }
+
+    myGirl.job = 'Waitress';
+
+    var myParents = {
+        mother: {
+            name: 'Terry Lev',
+            age: 65,
+            where: 'Sacramento, CA'
+        },
+        stepFather: {
+            name: 'Joe Lev',
+            age: 70,
+            where: 'Sacramento, CA'
+        }
+    }
+
+    var personArr = [];
+
+    personArr.push(me)
+    personArr.unshift(myGirl)
+    personArr.push(myParents)
+    console.log(personArr)
     console.log(me);
 
 })()
