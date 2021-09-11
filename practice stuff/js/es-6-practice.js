55bda1be10d599672c953138333ea4e5979c994d
nick adams <thebigdeal916@icloud.com>
2021-09-11 11:12:28 -0500

    exponent operator and let/const added

--- practice stuff/js/es-6-practice.js
+++ practice stuff/js/es-6-practice.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('hello');
+// es-6 example differences
+
+
+// Exponent operator //
+
+//es-5
+console.log(Math.pow(2, 8));
+
+//es-6
+console.log(2 ** 8);
+
+// let and const //
+
+//es-5 variable declaration
+
+var name = 'some name';
+
+//es-6 let and const declaration
+
+let name = 'some name';
+
+const newName = 'some name';
