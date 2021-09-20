f17d70f76a94ade1e360836ca9425376cfb2f204
nick adams <thebigdeal916@icloud.com>
2021-09-20 10:04:23 -0500

    added set/clear timeout

--- js/BOM-lec.js
+++ js/BOM-lec.js
@@ -1,23 +1,26 @@
 'use strict';
 
-// clear interval
+// set/clear interval
 var count = 0;
 var max = 10;
-var interval = 100;
+
 
 var intervalId = setInterval(function() {
     if(count >= max) {
         clearInterval(intervalId);
-        console.log('we done!')
+        console.log('we done!');
     } else {
         count++;
-        console.log('repeating ' + count)
+        console.log('repeating this line ' + count);
     }
-}, interval);
+}, 100);
 
 
-// set timeout
+// set/clear timeout
 var delay = 5000;
-var timeout2 = setTimeout(function() {
+var timeoutId = setTimeout(function() {
     alert('this is a delayed hello!');
 }, delay);
+
+// if we wanted to clear the set timeout above comment out the line below
+// clearTimeout(timeoutId);
