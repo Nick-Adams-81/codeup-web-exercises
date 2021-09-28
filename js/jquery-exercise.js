622ddfd17f2bc2adacdc5a4eaec6d31ea015e025
nick adams <thebigdeal916@icloud.com>
2021-09-28 12:02:17 -0500

    added header selector and alerted the contents

--- js/jquery-exercise.js
+++ js/jquery-exercise.js
@@ -3,7 +3,9 @@
 $(document).ready(function () {
     var li1 = $('#li-1').html()
     var li2 = $('#li-2').html()
-    alert(li1);
-    alert(li2);
+    $('ul').css('font-size', '20px')
     $('.codeup').css('border', '1px solid red');
+
+    var header = $('h1').html()
+    alert(header)
 });
