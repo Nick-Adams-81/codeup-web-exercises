f1cbaaa665148c58768aa7724f43ae19d290b241
nick adams <thebigdeal916@icloud.com>
2021-09-28 13:40:50 -0500

    added event listener

--- js/jquery-exercise.js
+++ js/jquery-exercise.js
@@ -2,9 +2,9 @@
 
 $(document).ready(function () {
 
-    $('ul').css('font-size', '20px')
-    $('.codeup').css('border', '1px solid red')
-    var header = $('h1').html()
-    alert(header)
-    $('h1', 'p', 'li').css('background-color', 'green')
+   $('h1').on('click', function() {
+       $(this).css('background-color', 'blue')
+   })
+
+
 });
