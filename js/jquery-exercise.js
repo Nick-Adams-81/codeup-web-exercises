89d152ad74fffebbd05f39307a4f9d13a69683e1
nick adams <thebigdeal916@icloud.com>
2021-09-28 13:32:35 -0500

    added multiple selectors

--- js/jquery-exercise.js
+++ js/jquery-exercise.js
@@ -1,11 +1,10 @@
 'use strict'
 
 $(document).ready(function () {
-    var li1 = $('#li-1').html()
-    var li2 = $('#li-2').html()
-    $('ul').css('font-size', '20px')
-    $('.codeup').css('border', '1px solid red');
 
+    $('ul').css('font-size', '20px')
+    $('.codeup').css('border', '1px solid red')
     var header = $('h1').html()
     alert(header)
+    $('h1', 'p', 'li').css('background-color', 'green')
 });
