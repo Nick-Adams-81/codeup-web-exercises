2be7395b3b8faa543d111ff14a9513443b5c0307
nick adams <thebigdeal916@icloud.com>
2021-09-29 12:11:00 -0500

    konami sound refactor

--- js/konami.js
+++ js/konami.js
@@ -1,7 +1,7 @@
 "use strict";
 
-// key variables
-var sound = new Audio('audio/cookie-down.mp3')
+// sound variable
+var sound = new Audio('audio/cookie-down.mp3');
 
 // correct konami code
 var thirtyLives = 'ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightbaEnter';
@@ -9,15 +9,16 @@ var thirtyLives = 'ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftA
 var code = '';
 
 $(document).keyup(function(event){
-    console.log(event.key)
+    console.log(event.key);
     // adding user input to empty string for comparison to konami code
     code += event.key;
     // comparing user input string to correct konami code
     if(code === thirtyLives) {
         alert('You have added 30 lives!');
         $('body').css('background-color', 'green');
-        $('h1').css('color', 'red');
-        sound.play()
+        $('h1').css('display', 'none');
+        $('img').attr('src', 'https://i.ytimg.com/vi/N2ACMD_SJtA/maxresdefault.jpg').css('height', '200px').css('width', '200px');
+        sound.play();
     }
 
 });
