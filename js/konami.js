f666e20d8f496fccd8a8019eb29ca8f652a63c5e
nick adams <thebigdeal916@icloud.com>
2021-09-29 19:54:32 -0500

    added second button

--- js/konami.js
+++ js/konami.js
@@ -3,6 +3,7 @@
 // sound variable
 var sound = new Audio('audio/cookie-down.mp3');
 $('#btn-2').css('display', 'none');
+$('#btn-3').css('display', 'none')
 // correct konami code
 var thirtyLives = 'ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightbaEnter';
 // empty string to store user input
@@ -18,9 +19,11 @@ $(document).keyup(function(event){
         alert('You have added 30 lives!');
         $('body').css('background-color', 'black');
         $('h1').css('display', 'none');
-        $('img').attr('src', 'https://dreamteck.io/images/blog/upld/mitko_contra_example.jpg').css('height', '600px').css('width', '100%').attr('class', 'animate__animated animate__fadeInRight').css('--animate-duration', '2.5s');
+        $('img').attr('src', 'https://dreamteck.io/images/blog/upld/mitko_contra_example.jpg').css('height', '500px').css('width', '100%').attr('class', 'animate__animated animate__fadeInRight').css('--animate-duration', '2.5s');
         $('#btn-1').css('display', 'none');
-        $('#btn-2').css('display', 'block').attr('class', 'animate__animated animate__fadeIn').css('--animate-duration', '2s');
+        $('#btn-2').css('display', 'block').attr('class', 'animate__animated animate__fadeIn').css('--animate-duration', '5s');
+        $('#btn-3').css('display', 'block').attr('class', 'animate__animated animate__fadeIn').css('--animate-duration', '5s')
+        $('#img2').attr('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYG5O09lUBdZvz33hXfaGIWzYlF_4gDW8sGg6Zx0NBqhe0FvbaT66FM43hBWlhuGEtsxw&usqp=CAU').css('height', '300px').css('width', '300px').css('margin-bottom', '50px').css('z-index', '1').css('border-radius', '10px').css('margin-left', '100px')
         sound.play();
     }
 
