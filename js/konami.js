a34938c396aac5bd9f44e40fcda1321e78216b72
nick adams <thebigdeal916@icloud.com>
2021-09-30 10:25:48 -0500

    added class to div element

--- js/konami.js
+++ js/konami.js
@@ -4,7 +4,7 @@
 var sound = new Audio('audio/cookie-down.mp3');
 var soundEffect = new Audio('audio/sound3.wav');
 
-// adding sound effect to button 1
+// adding sound effect to button 1 hover
 $('#btn-1').hover(function() {
     soundEffect.play();
 }, function() {
@@ -22,7 +22,7 @@ var thirtyLives = 'ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftA
 var code = '';
 document.documentElement.style.setProperty('--animate-duration', '.5s');
 
-$(document).keyup(function(event){
+$(document).keyup(function(event) {
 
     // adding user input to empty string for comparison to konami code
     code += event.key;
@@ -43,27 +43,26 @@ $(document).keyup(function(event){
         // konami logo
         $('#konami').attr('src', 'https://i.ytimg.com/vi/DIzNo9Y6408/hqdefault.jpg').css('height', '120px').css('width', '200px').css('padding-top', '5px').attr('class', 'animate__animated animate__fadeIn').css('--animate-duration', '6s').css('margin-bottom', '7px').css('margin-left', '500px').css('margin-top', '23px');
 
-        // hidig the try code again button
+        // hiding the try code again button
         $('#btn-1').css('display', 'none');
 
         // showing the player 1 button when code is inputted
         $('#btn-2').css('display', 'block').attr('class', 'animate__animated animate__fadeIn').css('--animate-duration', '7s').hover(function() {
-            soundEffect.play()
+            soundEffect.play();
         }, function() {
-            soundEffect.pause()
+            soundEffect.pause();
         });
 
         // showing player 2 button when code is inputted
         $('#btn-3').css('display', 'block').attr('class', 'animate__animated animate__fadeIn').css('--animate-duration', '7s').hover(function() {
-            soundEffect.play()
+            soundEffect.play();
         }, function() {
-            soundEffect.pause()
-        })
+            soundEffect.pause();
+        });
 
         // contra guys image
         $('#img2').attr('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYG5O09lUBdZvz33hXfaGIWzYlF_4gDW8sGg6Zx0NBqhe0FvbaT66FM43hBWlhuGEtsxw&usqp=CAU').css('height', '300px').css('width', '300px').css('margin-bottom', '60px').css('z-index', '1').css('border-radius', '10px').css('margin-left', '100px');
         // playng the sound when code is inputted
         sound.play();
     }
-
 });
