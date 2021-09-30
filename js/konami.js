5f115a79e53e06d1441a37cebf209f1d01224b51
nick adams <thebigdeal916@icloud.com>
2021-09-30 08:51:11 -0500

    added sound on button hover

--- js/konami.js
+++ js/konami.js
@@ -2,6 +2,7 @@
 
 // sound variable
 var sound = new Audio('audio/cookie-down.mp3');
+var soundEffect = new Audio('audio/sound3.wav');
 $('#btn-2').css('display', 'none');
 $('#btn-3').css('display', 'none')
 // correct konami code
@@ -25,6 +26,7 @@ $(document).keyup(function(event){
 
         // contra main logo
         $('img').attr('src', 'https://dreamteck.io/images/blog/upld/mitko_contra_example.jpg').css('height', '350px').css('width', '100%').attr('class', 'animate__animated animate__fadeInRight').css('--animate-duration', '2s').css('margin-top', '-20px')
+
         // konami logo
         $('#konami').attr('src', 'https://i.ytimg.com/vi/DIzNo9Y6408/hqdefault.jpg').css('height', '120px').css('width', '200px').css('padding-top', '5px').attr('class', 'animate__animated animate__fadeIn').css('--animate-duration', '6s').css('margin-bottom', '7px').css('margin-left', '500px').css('margin-top', '23px');
 
@@ -32,10 +34,18 @@ $(document).keyup(function(event){
         $('#btn-1').css('display', 'none');
 
         // showing the player 1 button when code is inputted
-        $('#btn-2').css('display', 'block').attr('class', 'animate__animated animate__fadeIn').css('--animate-duration', '7s');
+        $('#btn-2').css('display', 'block').attr('class', 'animate__animated animate__fadeIn').css('--animate-duration', '7s').hover(function() {
+            soundEffect.play()
+        }, function() {
+            soundEffect.pause()
+        });
 
         // showing player 2 button when code is inputted
-        $('#btn-3').css('display', 'block').attr('class', 'animate__animated animate__fadeIn').css('--animate-duration', '7s')
+        $('#btn-3').css('display', 'block').attr('class', 'animate__animated animate__fadeIn').css('--animate-duration', '7s').hover(function() {
+            soundEffect.play()
+        }, function() {
+            soundEffect.pause()
+        })
 
         // contra guys image
         $('#img2').attr('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYG5O09lUBdZvz33hXfaGIWzYlF_4gDW8sGg6Zx0NBqhe0FvbaT66FM43hBWlhuGEtsxw&usqp=CAU').css('height', '300px').css('width', '300px').css('margin-bottom', '60px').css('z-index', '1').css('border-radius', '10px').css('margin-left', '100px');
