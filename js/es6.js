6bd68efaeb19fb3d7af98826468ef68c1068847c
nick adams <thebigdeal916@icloud.com>
2021-10-13 11:50:18 -0500

    added map function

--- js/es6.js
+++ js/es6.js
@@ -33,29 +33,21 @@ const users = [
 // to the languages array
 // TODO: replace the `var` keyword with `const`, then try to reassign a variable
 // declared as `const`
-const name = 'your_name_here';
-const email = '';
-const languages = [];
+const name = 'Nick';
+const email = 'nick.pappas.adams@gmail.com';
+const languages = ['html', 'css', 'javascript'];
 
 
 // TODO: rewrite the object literal using object property shorthand
-users.push({
-    name,
-    email,
-    languages
-});
+users.push({name, email, languages});
 
 // TODO: replace `var` with `let` in the following variable declarations
 let emails = [];
 let names = [];
 
 // TODO: rewrite the following using arrow functions
-users.forEach((user) => {
-    return emails.push(user.email);
-});
-users.forEach((user) => {
-    return names.push(user.name);
-});
+users.forEach(user => emails.push(user.email));
+users.forEach(user => names.push(user.name));
 
 // TODO: replace `var` with `let` in the following declaration
 let developers = [];
@@ -63,12 +55,12 @@ users.forEach(function(user) {
     // TODO: rewrite the code below to use object destructuring assignment
     //       note that you can also use destructuring assignment in the function
     //       parameter definition
-    const person = {name: 'Nick', email: 'nick.pappas.adams@gmail.com', languages: ['html', 'css', 'javascript']}
 
+    const {name, email, languages} = user
 
     // TODO: rewrite the assignment below to use template strings
     //developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
-    developers.push(`${name} \s email is ${email} ${name} knows ${languages.join(', ')}`)
+    developers.push(`${name}'s email is ${email}. ${name} knows ${languages.join(', ')}`)
 });
 
 // TODO: Use `let` for the following variable
@@ -76,7 +68,7 @@ let list = '<ul>';
 
 // TODO: rewrite the following loop to use a for..of loop
 for (let developer of developers) {
-    list += `<li> developer </li>`
+    list += `<li> ${developer} </li>`
 }
 
 list += '</ul>';
